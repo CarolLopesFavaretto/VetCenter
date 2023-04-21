7e4ac41a6fa5cb53e33320fb5c571d5680579f3f
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2023-04-21 16:49:09 -0300

    Implementação de teste container de controller Guardian;

---
+++ src/test/java/com/vet/VetCenter/controller/GuardianController.java
@@ -0,0 +1,110 @@
+package com.vet.VetCenter.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vet.VetCenter.application.ports.in.GuardianService;
+import com.vet.VetCenter.config.PostgreSQLContainerTest;
+import com.vet.VetCenter.data.VetCenterData;
+import com.vet.VetCenter.domain.entity.Guardian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class GuardianController extends PostgreSQLContainerTest {
+
+    @Autowired
+    private GuardianService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void shouldCreatedGuardian() throws Exception {
+        Guardian guardian = VetCenterData.getGuardian();
+        service.create(guardian);
+
+        mvc.perform(MockMvcRequestBuilders.post("/guardian")
+                        .content(objectMapper.writeValueAsString(guardian))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+    }
+
+    @Test
+    public void shouldFindGuardianById() throws Exception {
+        Guardian guardian = VetCenterData.getGuardian();
+        service.create(guardian);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/guardian/{id}", guardian.getId().toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(mvcResult -> {
+                    String contentAsString = mvcResult.getResponse().getContentAsString();
+                    Guardian resp = objectMapper.readValue(contentAsString, Guardian.class);
+                    assertThat(resp.getName()).isEqualTo(guardian.getName());
+                    assertThat(resp.getCpf()).isEqualTo(guardian.getCpf());
+                    assertThat(resp.getTelephone()).isEqualTo(guardian.getTelephone());
+                });
+    }
+
+    @Test
+    public void shouldFindAllGuardian() throws Exception {
+        Guardian guardian = VetCenterData.getGuardian();
+        service.create(guardian);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/guardian")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldUpdateGuardian() throws Exception {
+        Guardian guardian = VetCenterData.getGuardian();
+        service.create(guardian);
+
+        mvc.perform(MockMvcRequestBuilders.put("/guardian/{id}", guardian.getId().toString())
+                        .content(objectMapper.writeValueAsString(guardian))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void shouldFindGuardianByIdNotFound() throws Exception {
+        Guardian guardian = VetCenterData.getGuardian();
+        service.create(guardian);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/guardian/{id}", 2)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldDeleteGuardian() throws Exception {
+        Guardian guardian = VetCenterData.getGuardian();
+        service.create(guardian);
+
+        mvc.perform(MockMvcRequestBuilders.delete("/guardian/{id}", guardian.getId().toString())
+                        .content(objectMapper.writeValueAsString(guardian))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+}
